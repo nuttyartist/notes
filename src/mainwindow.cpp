2b2a4cfe13be72ad846b6e97c344b06b06fb2dc9
Ali Diouri <alidiouri@gmail.com>
2017-06-11 20:17:21 +0000

    re issue #142
    Minor space modifications on Windows.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -231,7 +231,7 @@ void MainWindow::setupMainWindow ()
     ui->centralWidget->layout()->setContentsMargins(margins);
 #else
     ui->verticalSpacer->changeSize(0, 7, QSizePolicy::Fixed, QSizePolicy::Fixed);
-    ui->verticalSpacer_upEditorDateLabel->changeSize(0, 30, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    ui->verticalSpacer_upEditorDateLabel->changeSize(0, 27, QSizePolicy::Fixed, QSizePolicy::Fixed);
 #endif
     ui->frame->installEventFilter(this);
     ui->centralWidget->setMouseTracking(true);
