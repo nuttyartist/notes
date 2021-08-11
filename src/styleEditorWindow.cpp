0025c16e2837312af9562010e613dd90d1c19e72
nuttyartist <awesomeness.notes@gmail.com>
2021-08-11 13:38:33 +0300

    Set tooltips for Editor Settings

--- src/styleEditorWindow.cpp
+++ src/styleEditorWindow.cpp
@@ -26,6 +26,18 @@ StyleEditorWindow::StyleEditorWindow(QWidget *parent) :
 
 //    this->setStyleSheet(ss);
 
+    m_ui->serifButton->setToolTip("Change the text editor font to a serif font.\nClick again to try different fonts");
+    m_ui->sansSerifButton->setToolTip("Change the text editor font to a sans-serif font.\nClick again to try different fonts");
+    m_ui->monoButton->setToolTip("Change the text editor font to a monospace font.\nClick again to try different fonts");
+    m_ui->decreaseButton->setToolTip("Decrease font size");
+    m_ui->increaseButton->setToolTip("Increase font size");
+    m_ui->fullWidthButton->setToolTip("Make text stretch to the full width of the text editor");
+    m_ui->increaseWidthButton->setToolTip("Increase the text width by one character");
+    m_ui->decreaseWidthButton->setToolTip("Decrease the text width by one character");
+    m_ui->lightButton->setToolTip("Change app theme to Light");
+    m_ui->darkButton->setToolTip("Change app theme to Dark");
+    m_ui->sepiaButton->setToolTip("Chaneg app theme to Sepia");
+    m_ui->resetDefaultButton->setToolTip("Reset all to default settings");
 
     connect(m_ui->serifButton, &QPushButton::clicked, this, [this]{StyleEditorWindow::changeFontType(FontTypeface::Serif);});
     connect(m_ui->sansSerifButton, &QPushButton::clicked, this, [this]{StyleEditorWindow::changeFontType(FontTypeface::SansSerif);});
@@ -35,10 +47,10 @@ StyleEditorWindow::StyleEditorWindow(QWidget *parent) :
     connect(m_ui->fullWidthButton, &QPushButton::clicked, this, [this]{StyleEditorWindow::changeEditorTextWidth(EditorTextWidth::FullWidth);});
     connect(m_ui->increaseWidthButton, &QPushButton::clicked, this, [this]{StyleEditorWindow::changeEditorTextWidth(EditorTextWidth::Increase);});
     connect(m_ui->decreaseWidthButton, &QPushButton::clicked, this, [this]{StyleEditorWindow::changeEditorTextWidth(EditorTextWidth::Decrease);});
-    connect(m_ui->resetDefaultButton, &QPushButton::clicked, this, [this]{StyleEditorWindow::resetEditorToDefaultSettings();});
     connect(m_ui->lightButton, &QPushButton::clicked, this, [this]{StyleEditorWindow::changeThemeColor(ThemeColor::Light);});
     connect(m_ui->darkButton, &QPushButton::clicked, this, [this]{StyleEditorWindow::changeThemeColor(ThemeColor::Dark);});
     connect(m_ui->sepiaButton, &QPushButton::clicked, this, [this]{StyleEditorWindow::changeThemeColor(ThemeColor::Sepia);});
+    connect(m_ui->resetDefaultButton, &QPushButton::clicked, this, [this]{StyleEditorWindow::resetEditorToDefaultSettings();});
 }
 
 StyleEditorWindow::~StyleEditorWindow()
