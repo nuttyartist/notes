6c245a6b936b0ef340391d49fa27a97e1586fc5e
nuttyartist <awesomeness.notes@gmail.com>
2024-06-23 01:24:04 +0300

    Fix lqtutils for clang format

--- src/lqtutils_enum.h
+++ src/lqtutils_enum.h
@@ -30,25 +30,25 @@
 #include <QObject>
 #include <QQmlEngine>
 
-#define L_DECLARE_ENUM(enumName, ...)                                                          \
-    namespace enumName {                                                                       \
-    Q_NAMESPACE                                                                                \
-    enum Value { __VA_ARGS__ };                                                                \
-    Q_ENUM_NS(Value)                                                                           \
-    inline int qmlRegister##enumName(const char *uri, int major, int minor)                    \
-    {                                                                                          \
-        return qmlRegisterUncreatableMetaObject(enumName::staticMetaObject, uri, major, minor, \
-                                                #enumName, "Access to enums & flags only");    \
-    }                                                                                          \
-    inline int qRegisterMetaType()                                                             \
-    {                                                                                          \
-        return ::qRegisterMetaType<enumName::Value>(#enumName);                                \
-    }                                                                                          \
-    inline void registerEnum(const char *uri, int major, int minor)                            \
-    {                                                                                          \
-        enumName::qmlRegister##enumName(uri, major, minor);                                    \
-        enumName::qRegisterMetaType();                                                         \
-    }                                                                                          \
-    }
+#define L_DECLARE_ENUM(enumName, ...)                                                      \
+  namespace enumName {                                                                     \
+  Q_NAMESPACE                                                                              \
+  enum Value { __VA_ARGS__ };                                                              \
+  Q_ENUM_NS(Value)                                                                         \
+  inline int qmlRegister##enumName(const char *uri, int major, int minor)                  \
+  {                                                                                        \
+    return qmlRegisterUncreatableMetaObject(enumName::staticMetaObject, uri, major, minor, \
+                                            #enumName, "Access to enums & flags only");    \
+  }                                                                                        \
+  inline int qRegisterMetaType()                                                           \
+  {                                                                                        \
+    return ::qRegisterMetaType<enumName::Value>(#enumName);                                \
+  }                                                                                        \
+  inline void registerEnum(const char *uri, int major, int minor)                          \
+  {                                                                                        \
+    enumName::qmlRegister##enumName(uri, major, minor);                                    \
+    enumName::qRegisterMetaType();                                                         \
+  }                                                                                        \
+  }
 
 #endif // LQTUTILS_ENUM_H
