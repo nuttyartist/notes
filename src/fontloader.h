64105fe30ad7fb74a416894df3693aa52436e382
fangyefeng <1250890838@qq.com>
2024-11-01 16:48:50 +0800

    Avoid FontLoader Class from being constructed and copied elsewhere

--- src/fontloader.h
+++ src/fontloader.h
@@ -7,14 +7,17 @@
 class FontLoader
 {
 public:
-    FontLoader() = default;
-    ~FontLoader() = default;
-
     static FontLoader &getInstance()
     {
         static FontLoader instance;
         return instance;
     }
 
+    FontLoader(const FontLoader &) = delete;
+    void operator=(const FontLoader &) = delete;
     QFont loadFont(const QString &family, const QString &style, int pointSize);
+
+private:
+    FontLoader() = default;
+    ~FontLoader() = default;
 };
