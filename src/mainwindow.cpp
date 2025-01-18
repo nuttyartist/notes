38fd59b363d39aea1ce99ef72ef13216e36e8a6a
zjeffer <4633209+zjeffer@users.noreply.github.com>
2025-01-18 22:20:06 +0100

    Fix setActiveWindow deprecation warning

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3918,10 +3918,7 @@ bool MainWindow::eventFilter(QObject *object, QEvent *event)
     }
     case QEvent::ActivationChange: {
         if (m_editorSettingsWidget->isHidden()) {
-            QApplication::setActiveWindow(
-                    this); // TODO: The docs say this function is deprecated but it's the only one
-                           // that works in returning the user input from m_editorSettingsWidget
-                           // Qt::Popup
+            QWidget::activateWindow();
             m_textEdit->setFocus();
         }
         break;
